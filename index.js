20a3ebd0e7507b9da9524a9987902f1e4a1f620a
bernst4315 <bernst4315@gmail.com>
2024-12-09 15:00:41 -0700

    grabed the unique learner id and placed into an obj.
    successfully placed objs into the results array

---
+++ index.js
@@ -0,0 +1,129 @@
+// The provided course information.
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript",
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
+  ],
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
+    },
+  },
+];
+
+function getLearnerData(course, ag, submissions) {
+  // CourseInfo purpose is for data verification related to AssignmenGroup
+  //need to loop through array and grab unique learner id, w/out repeating values and place in new object
+
+  // const id = submissions[0];
+  // id.learner_id;
+  // console.log(id);
+  const result = [];
+  let uniqueId = 0;
+  
+  for (let i = 0; i < submissions.length; i++) {
+    //console.log(submissions[i].learner_id)
+    const learnerId = submissions[i].learner_id;
+    
+
+    if(learnerId !== uniqueId){
+      const testObj = { id: learnerId};
+      uniqueId = learnerId;
+      result.push(testObj);
+    }else{
+     continue; 
+    }
+    //const testObj = { id: learnerId};
+    ;
+  }
+
+  console.log(result)
+  //result.push(submissions);
+
+  return result;
+}
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+//console.log(result);
+//getLearnerData();
+
+// const result = [
+//   {
+//     id: 125,
+//     avg: 0.985, // (47 + 150) / (50 + 150)
+//     1: 0.94, // 47 / 50
+//     2: 1.0 // 150 / 150
+//   },
+//   {
+//     id: 132,
+//     avg: 0.82, // (39 + 125) / (50 + 150)
+//     1: 0.78, // 39 / 50
+//     2: 0.833 // late: (140 - 15) / 150
+//   }
+// ];
